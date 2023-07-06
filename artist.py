278c24011884409c399e7178d611099bfd13231a
darl1ng-design <84379309+darl1ng-design@users.noreply.github.com>
2023-07-06 13:36:16 +0200

    Add files via upload

---
+++ artist.py
@@ -0,0 +1,21 @@
+from req import *
+from requests import get
+def search_for_artist(token, artist_name):
+    url = "https://api.spotify.com/v1/search"
+    headers = auth_headers(token)
+    search_query = f"?q={artist_name}&type=artist&limit=1"
+
+    query_url = url + search_query
+    result = get(query_url, headers=headers)
+    json_res = json.loads(result.content)["artists"]["items"]
+    
+    if len(json_res) == 0: 
+        print(f"no artist with name of {artist_name} exists")
+        return None
+    
+    return json_res[0]['genres']
+    
+token=token(client_id, client_secret)
+res = search_for_artist(token, "gary moore")
+print(res)
+
---
+++ req.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+import os
+import base64
+from requests import post
+import json
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+def token(client_id, client_secret):
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_res = json.loads(result.content)
+    token = json_res["access_token"]
+    return token
+
+def auth_headers(token):
+    return {"Authorization": "Bearer " + token}
